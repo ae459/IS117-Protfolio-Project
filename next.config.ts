e2c69c8d7fd282045b7d4a322f6e535fc6967ea2
ae459 <ae459@njit.edu>
2026-05-08 08:32:58 -0400

    fix: correct basePath and assetPrefix to match repo name

--- next.config.ts
+++ next.config.ts
@@ -4,8 +4,8 @@ const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: isProd ? "/IS117-Portfolio-Project" : "",
-  assetPrefix: isProd ? "/IS117-Portfolio-Project/" : "",
+  basePath: isProd ? "/IS117-Protfolio-Project" : "",
+  assetPrefix: isProd ? "/IS117-Protfolio-Project/" : "",
   images: {
     unoptimized: true,
   },
